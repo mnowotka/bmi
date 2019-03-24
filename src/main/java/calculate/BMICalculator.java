d536fa0efd151df32159a67a43d6387c7b7948e0
mnowotka <mmmnow@gmail.com>
2019-03-24 13:29:38 +0100

    Scaffolding implemented.

---
+++ src/main/java/calculate/BMICalculator.java
@@ -0,0 +1,18 @@
+package calculate;
+
+public class BMICalculator implements Calculator {
+    private int weight;
+    private int height;
+
+    public BMICalculator(int height, int weight){
+
+    }
+
+    public String calculate(){
+        return "";
+    }
+
+    public String interpret(){
+        return "";
+    }
+}
